c38b9a94403bfd9d863ba3b9e6d0294ccb5ae532
AhmedAfzal2 <ahmedafzal6705@gmail.com>
2025-05-20 00:56:28 +0500

    .

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import infection as inf
 import igraph as ig
 import pandas as pd
 import math
@@ -11,41 +12,39 @@ def latlon_to_xy(lat, lon):
     y = R * math.log(math.tan(math.pi / 4 + math.radians(lat) / 2))
     return x, y
 
-def BFSTree(g):
-    q = [g.vs[0]]
-    visited = set()
-    
-    while len(q) > 0:
-        for edge in g.incident(q.pop(0)):
-            e = g.es[edge]
-            if e.target not in visited:
-                visited.add(e.target)
-                e['BFSTree'] = True
-                q.append(g.vs[e.target])
-
 def load_graph(node_file, edge_file):
     g = ig.Graph()
     
     with open(node_file) as node_list:
         reader = csv.reader(node_list)
         for row in reader:
-            g.add_vertex(name=row[1], country=row[2], lat=float(row[3]), lon=float(row[4]), population=int(row[5]), density=float(row[6]), hdi=float(row[7]))
+            g.add_vertex(name=row[1], country=row[2], lat=float(row[3]), lon=float(row[4]), population=int(row[5]), density=float(row[6]), hdi=float(row[7]), S = int(row[5]))
+    
+    g.vs['E'] = 0
+    g.vs['I'] = 0
+    g.vs['R'] = 0
+    g.vs['D'] = 0
 
     edge_list = pd.read_csv(edge_file, header=None)
-    edge_list.columns = ['src', 'dest', 'weight', 'type']
-    edges = list(zip(edge_list['src'] - 1, edge_list['dest'] - 1))
+    edge_list.columns = ['src', 'dest', 'weight', 'type', 'in_mst']
+    edges = list(zip(edge_list['src'], edge_list['dest']))
     
     g.add_edges(edges)
     g.es['weight'] = edge_list['weight'].tolist()
     g.es['type'] = edge_list['type'].tolist()
+    g.es['in_mst'] = edge_list['in_mst'].tolist()
     
-    BFSTree(g)
+    g.vs[3901]['S'] -= 10000
+    g.vs[3901]['E'] += 5000
+    g.vs[3901]['I'] += 5000
+    
     return g
 
 g = load_graph('data/node_list.csv', 'data/edge_list_type.csv')
 app = Flask(__name__)
 CORS(app)
 
+# initial graph sent to front-end
 @app.route('/graph')
 def getGraph():
     nodes = []
@@ -55,17 +54,69 @@ def getGraph():
             'id': v.index,
             'x': x,
             'y': y,
-            'size': 2,
+            'size': 3,
             'fixed': True
         })
     
     # takes edges in a spanning tree + all airport edges
-    edges = [
-        {'from': e.source, 'to': e.target}
-        for e in g.es if e['BFSTree'] or e['type'] == 'a'
-    ]
+    edges = []
+    for e in g.es:
+        if e['in_mst'] or e['type'] == 'a':
+            edges.append({'id': e.index, 'from': e.source, 'to': e.target})
     
     return jsonify({'nodes': nodes, 'edges': edges})
+
+# at every step, send updates to the graph
+# nodes = [3901]
+# visited = set()
+# visited.add(3901)
+@app.route('/update')
+def getUpdate():
+    # global nodes, visited
+    # toAdd = []
+    # ret = []
+    nodes = []
+
+    for v in g.vs:
+        if v['E'] + v['I'] > 0:
+            v['S'], v['E'], v['I'], v['R'], v['D'] = inf.get_next_city_step(v['S'], v['E'], v['I'], v['R'], v['D'], v['density'], v['hdi'])
+            darken = 1 - v['D'] / v['population']
+            transparent = (v['E'] + v['I']) / v['population']
+            color = f"rgba({int(255 * darken)}, 0, 0, {transparent})"
+            nodes.append({
+                'id': v.index,
+                'color': color
+            })
+    
+    changed = inf.travel(g)
+    for i in changed:
+        v = g.vs[i]
+        darken = 1 - v['D'] / v['population']
+        transparent = (v['E'] + v['I']) / v['population']
+        color = f"rgba({int(255 * darken)}, 0, 0, {transparent})"
+        nodes.append({
+            'id': v.index,
+            'color': color
+        })
+    # while len(nodes) > 0:
+    #     a = nodes.pop()
+    #     for ei in g.incident(a):
+    #         edge = g.es[ei]
+    #         neighbor = edge.source if edge.target == a else edge.target
+    #         if neighbor not in visited:
+    #             visited.add(neighbor)
+    #             toAdd.append(neighbor)
+    #             ret.append({
+    #                 'id': neighbor,
+    #                 'color': '#ff0000'
+    #             })
+
+    # for i in toAdd:
+    #     nodes.append(i)
+
+
+    return jsonify({'nodes': nodes})
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.js
+++ script.js
@@ -25,11 +25,18 @@ fetch(
         addCountryBorders(data, map);
 });
 
+// graph data
+fetch('http://localhost:5000/graph')
+    .then(response => response.json())
+    .then(data => {
+        drawGraph(data);
+    });
+
 const airportIcon = L.icon({
     iconUrl: 'icons/airport.svg',
     iconSize: [16, 16],
     iconAnchor: [8, 16]
-})
+});
 
 Papa.parse("data/filtered_airport_coords.csv", {
     download: true,
@@ -39,7 +46,7 @@ Papa.parse("data/filtered_airport_coords.csv", {
             L.marker(coords, {icon: airportIcon}).addTo(map);
         });
     }
-})
+});
 
 function addCountryBorders(data, map) {
     L.geoJSON(data, {
@@ -53,13 +60,13 @@ function borderStyle() {
         color: 'red',
         weight: 1,
         fillOpacity: 0
-    }
+    };
 }
 
 function highlightedStyle() {
     return {
         fillOpacity: 1
-    }
+    };
 }
 
 function addEvents(feature, layer) {
@@ -67,46 +74,89 @@ function addEvents(feature, layer) {
         mouseover: highlightCountry,
         mouseout: resetCountry,
         click: () => countryClick(feature)
-    })
+    });
 }
 
 function highlightCountry(e) {
-    e.target.setStyle(highlightedStyle())
+    e.target.setStyle(highlightedStyle());
 }
 
 function resetCountry(e) {
-    e.target.setStyle(borderStyle())
+    e.target.setStyle(borderStyle());
 }
 
 function countryClick(feature) {
-    console.log(feature.properties.name)
+    console.log(feature.properties.name);
 }
 
-const switchTab = document.getElementById('switch-tab')
-const mapDiv = document.getElementById('map')
-const graphDiv = document.getElementById('graph')
-switchTab.addEventListener('click', () => {
-    mapDiv.classList.toggle("active")
-    graphDiv.classList.toggle("active")
-    console.log('switch')
-})
+const switchTab = document.getElementById('switch-tab');
+const mapDiv = document.getElementById('map');
+const graphDiv = document.getElementById('graph');
+const graph = new graphology.Graph();
+let firstSwitch = true;
 
-fetch('http://localhost:5000/graph')
-    .then(response => response.json())
-    .then(data => {
-        drawGraph(data);
-    })
-
-
-
+// creates graph and initializes renderer
 function drawGraph(data) {
-    const graph = new graphology.Graph();
     data.nodes.forEach(n => {
         graph.addNode(n.id, n);
     });
     data.edges.forEach(e => {
-        graph.addEdge(e.from, e.to);
+        try {
+            graph.addEdgeWithKey(e.id, e.from, e.to);
+        }
+        catch {}
     });
 
-    const renderer = new Sigma(graph, graphDiv);
+    const renderer = new Sigma(graph, graphDiv, {
+        allowInvalidContainer: true,
+        mouseEnabled: false
+    });
+
+    switchTab.addEventListener('click', () => {
+        onSwitchTabClick(renderer)
+    });
+
+    graph.setNodeAttribute(3901, 'color', '#ff0000')
 }
+
+function onSwitchTabClick(renderer) {
+    mapDiv.classList.toggle("active")
+    graphDiv.classList.toggle("active")
+    if (graphDiv.classList.contains("active")) {
+        setTimeout(() => {
+            if (firstSwitch) {
+                firstSwitch = false;
+                clampCamera(renderer)
+            }
+            renderer.refresh();
+        }, 100);
+    }
+}
+
+// fix the camera in one spot
+function clampCamera(renderer) {
+    // const camera = renderer.getCamera();
+    // camera.setState({
+    //         x: 0.492,
+    //         y: 0.533
+    //     });
+    // camera.on("updated", () => {
+    //     camera.setState({
+    //         x: 0.492,
+    //         y: 0.533,
+    //         ratio: 1
+    //     });
+    // });
+}
+
+const step = document.getElementById('step');
+
+step.addEventListener('click', () => {
+    fetch('http://localhost:5000/update')
+        .then(response => response.json())
+        .then(data => {
+            data.nodes.forEach(n => {
+                graph.setNodeAttribute(n.id, 'color', n.color);
+            })
+        });
+});
